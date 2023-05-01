99de4a4c6a039e61f6be04e90490d17129c2bcaf
moheb_maher <dvm.moheb.maher@gmail.com>
2023-05-01 20:16:06 +0300

    Update validate_model.py

--- Assignment-05/helpers/validate_model.py
+++ Assignment-05/helpers/validate_model.py
@@ -1,10 +1,14 @@
 def compare_models(X, y, scoring, estimators={}, **kwargs):
+    
     "Compare different estimators."
+    
     import matplotlib.pyplot as plt
     from sklearn.model_selection import cross_val_score, KFold
+    
     estimators.update(kwargs)
     results = []
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    
     for name, estimator in estimators.items():
         cv_results = cross_val_score(estimator, X, y, cv=kf, scoring=scoring)
         print(f"For {name}, results are:")
@@ -12,6 +16,7 @@ def compare_models(X, y, scoring, estimators={}, **kwargs):
         print(f"  mean = {cv_results.mean():.2%}")
         print(f"  max = {cv_results.max():.2%}\n")
         results.append(cv_results)
+        
     plt.boxplot(results, labels=estimators.keys())
     plt.xticks(rotation=90)
     plt.show()
